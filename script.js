d30574bdfc1a0bb76a99afca31ce64e273e43e42
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-13 09:42:55 +0100

    Carousel finish

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
- let buttonLeft = document.querySelector('#buttonLeft');
- let buttonRight = document.querySelector('#buttonRight');
-let imgArray = ['img1','img2','img3','img4','img5','img6','img7','img8','img9',
-'img10','img11','img12'];
+let buttonLeft = document.querySelector('#buttonLeft');
+let buttonRight = document.querySelector('#buttonRight');
+let imgArray = ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7', 'img8', 'img9',
+    'img10', 'img11', 'img12'];
 let div = document.querySelector('.photo');
 
 
 //function for load the image
-function load () {
-     for (let i = 0; i < imgArray.length; i++) {
-         //create a div for stock the images
+function load() {
+    for (let i = 0; i < imgArray.length; i++) {
+        //create a div for stock the images
         div.style.backgroundImage = "url('/" + imgArray[i] + ".jpg')";
     }
 }
 
- buttonLeft.addEventListener('click', function (){
+let j = 0;
+load();
+div.style.backgroundImage = "url('/" + imgArray[j] + ".jpg')";
 
- });
+buttonLeft.addEventListener('click', function (){
+    j--;
+    load();
+    div.style.backgroundImage = "url('/" + imgArray[j] + ".jpg')";
+    console.log(j)
+    if (j === -1) {
+        j = imgArray.length -1;
+        load();
+        div.style.backgroundImage = "url('/" + imgArray[j] + ".jpg')";
+    }
+});
 
- buttonRight.addEventListener('click', function (){
-     for (let i = 0; i < imgArray[i].length; i++) {
-         if (i < imgArray.length) {
-             div.style.backgroundImage = "url('/"+ imgArray[i] +".jpg')";
-             div.style.transition = "all 0.5s ease";
-             console.log("gkbj");
-             i++;
-         }
-         else if (i === imgArray.length) {
-             console.log("nbvc");
-         }
-     }
- });
 
-setInterval(function (){
-  //boucle for
-},1000);
+
+buttonRight.addEventListener('click', function (){
+    j++;
+    load();
+    div.style.backgroundImage = "url('/" + imgArray[j] + ".jpg')";
+
+    if (j === imgArray.length - 1) {
+        j = -1;
+    }
+});
+
+let l = 0;
+
+// setInterval(function () {
+//     load();
+//     l++;
+//     div.style.backgroundImage = "url('/" + imgArray[l] + ".jpg')";
+//     if (l === imgArray.length - 1) {
+//         l = 0;
+//     }
+// }, 10000);
