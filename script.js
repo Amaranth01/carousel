664fb293a5484cb6ff6efc644cb3294df4fd855e
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-13 11:53:42 +0100

    problem with githubpage

--- script.js
+++ script.js
@@ -9,30 +9,30 @@ let div = document.querySelector('.photo');
 function load() {
     for (let i = 0; i < imgArray.length; i++) {
         //create a div for stock the images
-        div.style.backgroundImage = "url('/" + imgArray[i] + ".jpg')";
+        div.style.backgroundImage = "url('" + imgArray[i] + ".jpg')";
     }
 }
 
 let j = 0;
 load();
-div.style.backgroundImage = "url('/" + imgArray[j] + ".jpg')";
+div.style.backgroundImage = "url('" + imgArray[j] + ".jpg')";
 
 buttonLeft.addEventListener('click', function (){
     j--;
     load();
-    div.style.backgroundImage = "url('/" + imgArray[j] + ".jpg')";
+    div.style.backgroundImage = "url('" + imgArray[j] + ".jpg')";
     // condition for go back
     if (j === -1) {
         j = imgArray.length -1;
         load();
-        div.style.backgroundImage = "url('/" + imgArray[j] + ".jpg')";
+        div.style.backgroundImage = "url('" + imgArray[j] + ".jpg')";
     }
 });
 
 buttonRight.addEventListener('click', function (){
     j++;
     load();
-    div.style.backgroundImage = "url('/" + imgArray[j] + ".jpg')";
+    div.style.backgroundImage = "url('" + imgArray[j] + ".jpg')";
     //condition for go ahead
     if (j === imgArray.length - 1) {
         j = -1;
@@ -42,7 +42,7 @@ buttonRight.addEventListener('click', function (){
 setInterval(function () {
     load();
     j++;
-    div.style.backgroundImage = "url('/" + imgArray[j] + ".jpg')";
+    div.style.backgroundImage = "url('" + imgArray[j] + ".jpg')";
     if (j === imgArray.length - 1) {
         j = 0;
     }
